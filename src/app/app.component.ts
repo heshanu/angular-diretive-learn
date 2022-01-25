e33105ad0f504a53bc0b8e878234600ec0a83f53
heshanu <heshanu97@gmail.com>
2022-01-26 00:33:03 +0530

    2 files modified-solve problem in pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-currentPage=0;
+currentPage=1;
  images=[
    {title:'at the beach',url:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
    {title:'girl',url:'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'},
    {title:'friend',url:'https://media.istockphoto.com/photos/orangutan-picture-id1293750129?b=1&k=20&m=1293750129&s=170667a&w=0&h=Q1oqdBvKys_zug09v0W4MWJue-88tW8R4O20j64M3hw='},
    {title:'friend',url:'https://media.istockphoto.com/photos/orangutan-picture-id1293750129?b=1&k=20&m=1293750129&s=170667a&w=0&h=Q1oqdBvKys_zug09v0W4MWJue-88tW8R4O20j64M3hw='}
  ];
+
+ 
 }
